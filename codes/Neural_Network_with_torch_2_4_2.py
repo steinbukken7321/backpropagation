067c229f5db232bfb3737e3e6d2e807fc49594e5
Rafael Ziani de Carvalho <rafael.ziani1@gmail.com>
2024-04-22 10:16:51 -0300

    2_4_2

--- codes/Neural_Network_with_torch_2_4_2.py
+++ codes/Neural_Network_with_torch_2_4_2.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 import torch.optim as optim
 
 # definição da classe da rede neural
+
+
 class RedeNeural(nn.Module):
     def __init__(self, input_size, hidden_size, output_size, pesos_oculta=None,
                  pesos_saida=None, bias_oculta=None, bias_saida=None):
@@ -17,14 +19,14 @@ class RedeNeural(nn.Module):
             self.camada_oculta.weight = nn.Parameter(
                 torch.tensor(pesos_oculta))
         if pesos_saida is not None:
-            self.camada_saida.weight = nn.Parameter(torch.tensor(pesos_saida))
+            # Transpondo os pesos da camada de saída
+            self.camada_saida.weight = nn.Parameter(torch.tensor(pesos_saida).t())
 
         # inicialização dos bias manualmente, se fornecidos
         if bias_oculta is not None:
             self.camada_oculta.bias = nn.Parameter(torch.tensor(bias_oculta))
         if bias_saida is not None:
             self.camada_saida.bias = nn.Parameter(torch.tensor(bias_saida))
-
     # (forward pass)
     def forward(self, x):
         # Ativação da camada oculta (sigmoid)
@@ -34,15 +36,18 @@ class RedeNeural(nn.Module):
         return x
 
 # função para treinar a rede neural
+
+
 def treinar_rede(modelo, entradas, saidas, epochs=10000, lr=0.5):
     outputs_iniciais = modelo(entradas)
     # Definindo a função de perda
+
     def minha_funcao_perda(saidas, outputs_iniciais):
         loss_1 = 1/2 * (saidas[0, 0] - outputs_iniciais[0, 0])**2
         loss_2 = 1/2 * (saidas[0, 1] - outputs_iniciais[0, 1])**2
         perda_total = loss_1 + loss_2
         return perda_total, loss_1, loss_2
-    
+
     # definição do otimizador (SGD - Gradiente Descendente Estocástico)
     optimizer = optim.SGD(modelo.parameters(), lr=lr)
 
@@ -51,7 +56,7 @@ def treinar_rede(modelo, entradas, saidas, epochs=10000, lr=0.5):
     print(f'erro 1 antes do treinamento: {loss_1:.8f}')
     print(f'erro 2 antes do treinamento: {loss_2:.8f}')
     print(f'erro total antes do treinamento: {perda_total:.8f}\n')
-    
+
     criterion = nn.MSELoss()
 
     # Loop de treinamento
@@ -69,11 +74,15 @@ def treinar_rede(modelo, entradas, saidas, epochs=10000, lr=0.5):
 
         # a cada x épocas, imprime informações sobre a perda e os valores de saída
         if (epoch+1) % 10000 == 0:
-            perda_total = 1/2 * ((saidas[0, 0] - outputs[0, 0])**2 + (saidas[0, 1] - outputs[0, 1])**2)
-            print(f'Época [{epoch+1}/{epochs}], erro total após treinamento: {perda_total:.9f}')
+            perda_total = 1/2 * \
+                ((saidas[0, 0] - outputs[0, 0])**2 +
+                 (saidas[0, 1] - outputs[0, 1])**2)
+            print(
+                f'Época [{epoch+1}/{epochs}], erro total após treinamento: {perda_total:.9f}')
             # Imprimindo os valores de saída o1 e o2 após o treinamento
             print("Saída 1 após o treinamento:", format(outputs[0, 0], '.8f'))
             print("Saída 2 após o treinamento:", format(outputs[0, 1], '.8f'))
+
 
 if __name__ == "__main__":
     # Definindo manualmente a entrada
@@ -83,24 +92,29 @@ if __name__ == "__main__":
     saidas = torch.tensor([[0.01, 0.99]])
 
     # Valores de pesos e bias manualmente definidos
-    pesos_oculta = [[0.15, 0.2],
-                    [0.25, 0.3]]
+    pesos_oculta = [[0.15, 0.25],
+                    [0.2, 0.3],
+                    [0.25, 0.35],
+                    [0.3, 0.4]]
 
     pesos_saida = [[0.4, 0.45],
-                   [0.5, 0.55]]
+                   [0.5, 0.55],
+                   [0.6, 0.65],
+                   [0.7, 0.75]]
 
-    bias_oculta = [0.35, 0.35]
+    bias_oculta = [0.35, 0.35, 0.35, 0.35]
     bias_saida = [0.6, 0.6]
 
     # modelo da rede neural
-    modelo = RedeNeural(input_size=2, hidden_size=2, output_size=2,
+    modelo = RedeNeural(input_size=2, hidden_size=4, output_size=2,
                         pesos_oculta=pesos_oculta, pesos_saida=pesos_saida,
                         bias_oculta=bias_oculta, bias_saida=bias_saida)
-    
+
     # imprimindo as saídas o1 e o2 antes do treinamento
-    print("Saída 1 antes do treinamento:", format(modelo(entradas)[0, 0], '.8f'))
-    print("Saída 2 antes do treinamento:", format(modelo(entradas)[0, 1], '.8f'))
+    print("Saída 1 antes do treinamento:",
+          format(modelo(entradas)[0, 0], '.8f'))
+    print("Saída 2 antes do treinamento:",
+          format(modelo(entradas)[0, 1], '.8f'))
 
     # treinando a rede neural
     treinar_rede(modelo, entradas, saidas)
-
